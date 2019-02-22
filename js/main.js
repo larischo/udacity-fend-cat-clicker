15a440c9941fe33dab6e6f65163d9b467b4e7294
i00o8j <larissacatherine.schoell.extern@ww-informatik.de>
2019-02-22 17:08:22 +0100

    version 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-let count=0;
-$('.cats_img').click(function(e) {
-    $('.clicks')[0].innerHTML="clicks: "+ ++count;
+let count_1=0;
+let count_2=0;
+$('#cats_1').click(function(e) {
+    $('#click_1')[0].innerHTML="clicks: "+ ++count_1;
+  });
+  $('#cats_2').click(function(e) {
+    $('#click_2')[0].innerHTML="clicks: "+ ++count_2;
   });
